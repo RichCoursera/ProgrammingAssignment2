3702f5f6d1607c1fe44060562835b0aab1342a94
RichCoursera <RichCoursera@users.noreply.github.com>
2015-02-17 22:35:50 -0700

    Updates to Week 3 project -
    Uploaded my code o handle the inverse of a square matrix that is not singular.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below will take a matrix and return
+## the inverse of that matix.
+##There is also a function for an error message for invalid data.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object 
+## that caches its inverse.
+makeCacheMatrix <- function(x = matrix() )
+{
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}  
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. If the inverse has already
+## been calculated (and the matrix has not changed), then the 
+## cacheSolve will retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) 
+{
+  m <- x$getinverse()
+  if(!is.null(m)) 
+    {
+    message("getting cached data")
+    return(m)
+    }
+  data <- x$get()
+  ##m <- solve(data, ...)
+  ## credit for the tryCatch goes to the URL: http://stackoverflow.com/questions/3440373/functions-and-try-in-r
+  tryCatch(m <- solve(data, ...), error=function(e)
+    {print(errmsg()) ; return()} )
+  x$setinverse(m)
+  m
+}
+  
+## Error message handling
+errmsg<- function()
+{
+    message("The data entered was not valid. It was either")
+    message("a singular matrix or not a squre matrix")
+    message("PS. I cannot figure out how to stop the function")
+    message("before the two NULLs show below :)")
+    return()
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
